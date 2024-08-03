b61f092f2f2431c834c3d0126bc273e38f31be43
nova1751 <nova2319427728@outlook.com>
2024-08-04 01:23:18 +0800

    chore: disable the incorrect error in router file

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,10 @@
-import { createElement, lazy } from 'react'
+/* eslint-disable react-refresh/only-export-components */
+import { lazy } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import { Router } from '@remix-run/router'
 
 const VirtualListDemo1 = lazy(() => import('@/views/virtual-list-demo-1'))
 
-const router: Router = createBrowserRouter([
-  { path: '/', element: createElement(VirtualListDemo1) }
-])
+const router: Router = createBrowserRouter([{ path: '/', element: <VirtualListDemo1 /> }])
 
 export default router
